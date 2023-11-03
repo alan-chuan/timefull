19b91ad9d7aebce2ed0cdcb734fadf451c339e02
Alan Chuan <chuan2@wisc.edu>
2023-11-03 17:09:06 -0400

    lift state up to home component

--- app/page.js
+++ app/page.js
@@ -2,15 +2,20 @@
 
 import Sidebar from "@/components/Sidebar";
 import Dashboard from "@/components/Dashboard";
-
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [selectedDay, setSelectedDay] = useState();
+  useEffect(() => {
+    const today = new Date().toLocaleDateString();
+    setSelectedDay(today);
+  }, []);
 
   return (
     <main>
       <div className="flex h-screen">
-        <Sidebar />
-        <Dashboard />
+        <Sidebar selectedDay={selectedDay} setSelectedDay={setSelectedDay} />
+        <Dashboard selectedDay={selectedDay} setSelectedDay={setSelectedDay} />
       </div>
     </main>
   );
